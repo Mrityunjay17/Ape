88fbb1c3e2bea010a5a14d3802ad9cac8623ef46
Mrityunjay Pandey <17mrityunjaypandey@gmail.com>
2017-11-06 15:46:32 +0530

    Complete Login from server

--- src/Login/Login.java
+++ src/Login/Login.java
@@ -5,36 +5,80 @@ package Login;
 import HttpClient.HttpPost;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import org.json.JSONObject;
 
+import java.io.*;
 import java.net.URL;
 
 
-public class Login {
 
+public class Login{
     @FXML
     TextField EmailID;
     @FXML
     PasswordField Password;
+    @FXML
+    Label  errormessage;
 
+
+    public static Stage AdminLoginStage;
 
     @FXML
     void Login(ActionEvent event) throws Exception {
-       login();
-        }
+       login(event);
 
-    private void login() {
+    }
+
+    private void login(ActionEvent event) {
         try {
-            URL url = new URL("http://localhost/loginn");
+            URL url = new URL("http://localhost/login");
             JSONObject jsonObject=new JSONObject();
             jsonObject.put("EmailId",EmailID.getText());
             jsonObject.put("Password",Password.getText());
             HttpPost httpPost=new HttpPost(url,jsonObject);
+
+          if (httpPost.getStringbuffer().toString().equals("User Not Found")){
+              errormessage.setText(httpPost.getStringbuffer().toString());
+          }
+          else if(httpPost.getStringbuffer().toString().equals("Please check your password"))
+          {
+              errormessage.setText(httpPost.getStringbuffer().toString());
+          }
+          else {
+              writeNewUser(httpPost.getStringbuffer().toString());
+              Parent AdminLoginParent = FXMLLoader.load(getClass().getResource("/Account/Main.fxml"));
+              Scene AdminLoginScene = new Scene(AdminLoginParent);
+              AdminLoginStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+              AdminLoginStage.setScene(AdminLoginScene);
+              AdminLoginStage.setMaximized(true);
+              AdminLoginStage.show();
+          }
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
+
+    public void writeNewUser(String token) {
+
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter("token.ddf", "UTF-8");
+            writer.println(token);
+            writer.close();
+        } catch (Exception e){
+
         }
 
     }
+
+
+
 }
