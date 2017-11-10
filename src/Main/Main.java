62c1406f5010478f02ce571d7f3f9370c319291b
Mrityunjay Pandey <17mrityunjaypandey@gmail.com>
2017-11-10 18:12:55 +0530

    Main in Add Start page

---
+++ src/Main/Main.java
@@ -0,0 +1,28 @@
+package Main;
+
+import Utilitypackage.Utility;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("ConnectionCheck.fxml"));
+        primaryStage.getIcons().add(new Image("/Resources/images/ApeLogo.png"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+       // Utility.check();
+        //System.out.println(Utility.getTitle());
+    }
+}
