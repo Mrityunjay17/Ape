046d071fe25c52eb6af6e2d19fac81356cd2ab5e
Mrityunjay <17mrityunjaypandey@gmail.com>
2017-12-01 00:18:16 +0530

    remove complexity

--- src/Admin/CreateAdmin.java
+++ src/Admin/CreateAdmin.java
@@ -5,30 +5,34 @@ import controls.NumberField;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 import org.json.JSONObject;
+import Utility.Verification;
 
 import java.net.URL;
 
 
 public class CreateAdmin{
-
-    String Add_border ="-fx-border-width: 1,1,1,1;-fx-border-color: red";
-    String Remove_border ="-fx-border-width: 1,1,1,1";
-    String Email_Pattern="(^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,4}$)";
-    String Mobile_Pattern="[0-9]{10}";
+    Verification verification =new Verification();
+   //  boolean nullAdmin;
     ObservableList<String> gender= FXCollections.observableArrayList("Male","Female","Other");
-
+    private Stage Admin_Stage;
     @FXML
     ComboBox<String> Gender;
     @FXML
-    TextField F_name,Mname,Lname,Gfname,Gmname,Glname, Email_id;
+    TextField fName,Mname,Lname, gFName,Gmname,Glname, Email_id;
     @FXML
     DatePicker Date_of_birth;
     @FXML
@@ -36,23 +40,26 @@ public class CreateAdmin{
     @FXML
     PasswordField password,Confirm_Password;
 
+
+
     @FXML
     void CreateAccount(ActionEvent event) {
-        data_check();
+        data_check(event);
+
     }
 
     @FXML
     void createAccount(KeyEvent keyEvent){
         if(keyEvent.getCode()== KeyCode.ENTER) {
-            data_check();
+            data_check(keyEvent);
         }
     }
 
-    private void data_check(){
+    private void data_check( Event event){
         try {
             JSONObject data=getData();
             if (data.length()!=0){
-                send_to_server(data);
+                send_to_server(data,event);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -64,14 +71,13 @@ public class CreateAdmin{
 
         JSONObject Create_account=new JSONObject();
 
-        if(!F_name.getText().equals("")&&!Gfname.getText().equals("")&&!Gender.getValue().equals(null)
-                && Email_id.getText().matches(Email_Pattern)&&Mobile.getText().matches(Mobile_Pattern)&&
-                password.getText().length()>=6 &&password.getText().equals(Confirm_Password.getText())){
-
-            Create_account.put("First_Name", F_name.getText());
+        if(verification.textFieldNotNull(fName,gFName)&& verification.datePickerNotNull(Date_of_birth)
+                &&verification.comboBoxNotNull(Gender)&&verification.mobileVerify(Mobile)&&
+                verification.emailVerify(Email_id)&& verification.passwordVerify(password,Confirm_Password)){
+            Create_account.put("First_Name", fName.getText());
             Create_account.put("Middle_Name",Mname.getText());
             Create_account.put("Last_Name",Lname.getText());
-            Create_account.put("Guardian_First_name",Gfname.getText());
+            Create_account.put("Guardian_First_name", gFName.getText());
             Create_account.put("Guardian_Middle_name",Gmname.getText());
             Create_account.put("Guardian_Last_name",Glname.getText());
             Create_account.put("Date_Of_Birth", Date_of_birth.getValue());
@@ -80,110 +86,44 @@ public class CreateAdmin{
             Create_account.put("Mobile",Mobile.getText());
             Create_account.put("Password",password.getText());
         }
-
-        if(F_name.getText().equals("")){
-            F_name.setStyle(Add_border);
-        }
-        if(Gfname.getText().equals("")){
-            Gfname.setStyle(Add_border);
-        }
-        if(Email_id.getText().equals("")){
-            Email_id.setStyle(Add_border);
-        }
-        if(Mobile.getText().equals("")){
-            Mobile.setStyle(Add_border);
-        }
-        if(password.getText().length()<6){
-            password.setStyle(Add_border);
-            Confirm_Password.setStyle(Add_border);
-        }
-        if(Gender.getSelectionModel().isEmpty()){
-            Gender.setStyle(Add_border);
-        }
-        if(Date_of_birth.getValue()==null){
-            Date_of_birth.setStyle(Add_border);
-        }
         return Create_account;
     }
 
 
-    private void send_to_server(JSONObject jsonObject) throws Exception {
+    private void send_to_server(JSONObject jsonObject, Event event) throws Exception {
         URL url = new URL("http://localhost/create_admin");
-
         HttpPost httpPost=new HttpPost(url,jsonObject);
         System.out.println(httpPost.getStringbuffer().toString());
+
+        Parent Admin_Parent = FXMLLoader.load(getClass().getResource("Admin_home.fxml"));
+        Scene AdminLoginScene = new Scene(Admin_Parent);
+        Admin_Stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Admin_Stage.setScene(AdminLoginScene);
+        Admin_Stage.setResizable(true);
+        Admin_Stage.setMaximized(true);
+        Admin_Stage.show();
     }
 
 
     @FXML
-    public void initialize(){
-
-        Email_id.textProperty().addListener(((observable, oldValue, newValue) -> {
-            if(newValue.matches(Email_Pattern)){
-                Email_id.setStyle(Remove_border);
-            }
-            else {
-                Email_id.setStyle(Add_border);
-            }
-        }));
-
-        F_name.textProperty().addListener(((observable, oldValue, newValue) -> {
-            if(newValue.length()==0){
-                F_name.setStyle(Add_border);
-            }
-            else {
-                F_name.setStyle(Remove_border);
-            }
-        }));
-
-        Gfname.textProperty().addListener((observable, oldValue, newValue) -> {
-            if(newValue.length()==0){
-                Gfname.setStyle(Add_border);
-            }
-            else {
-                Gfname.setStyle(Remove_border);
-            }
-        });
-
-        Mobile.textProperty().addListener((observable, oldValue, newValue) -> {
-            if(newValue.length()>10){
-                Mobile.setText(oldValue);
-                Mobile.setStyle(Remove_border);
-            }
-            else if(newValue.matches(Mobile_Pattern)){
-                Mobile.setStyle(Remove_border);
-            }
-            else {
-                Mobile.setStyle(Add_border);
-            }
-        });
-
-        password.textProperty().addListener((observable, oldValue, newValue) -> {
-            if(newValue.length()<6){
-                password.setStyle(Add_border);
-            }
-            else {
-                password.setStyle(Remove_border);
-            }
-        });
-        Confirm_Password.textProperty().addListener((observable, oldValue, newValue) -> {
-            if(password.getText().equals(Confirm_Password.getText())){
-                Confirm_Password.setStyle(Remove_border);
-            }
-            else {
-                Confirm_Password.setStyle(Add_border);
-            }
-        });
-
-        Date_of_birth.valueProperty().addListener((observable, oldValue, newValue) -> {
-           Date_of_birth.setStyle(Remove_border);
-        });
-
-        Gender.valueProperty().addListener((observable, oldValue, newValue) -> {
-            Gender.setStyle(Remove_border);
-
-        });
-
+    private void initialize(){
+        verification.emailVerifyOnTextChange(Email_id);
+        verification.mobileVerifyOnNumberChange(Mobile);
+        verification.textFieldVerifyOnTextChange(fName,gFName);
+        verification.passwordFieldVerifyOnTextChange(password);
+        verification.confirmPasswordFieldOnTextChange(password,Confirm_Password);
+        verification.datePickerOnDateChange(Date_of_birth);
+        verification.comboBoxOnValueChange(Gender);
         Gender.setItems(gender);
+
+       /* try {
+            HttpGet httpGet=new HttpGet("http://localhost");
+            if(httpGet.getStringbuffer().toString().equals("0")){
+                nullAdmin=true;
+            }
+        }
+        catch (Exception e){
+
+        }*/
     }
 }
